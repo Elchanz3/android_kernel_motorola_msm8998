9fc95f942e3b0d76725bbb701b487ac70e3f04c6
snapdragon teaks <inutilidades639@gmail.com>
2024-10-02 00:12:22 -0300

    add a build script for evert

---
+++ evert.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+export PLATFORM_VERSION=14
+export ANDROID_MAJOR_VERSION=u
+export ARCH=arm64
+export SEC_BUILD_CONF_VENDOR_BUILD_OS=14
+export CDIR="$(pwd)"
+export AK3="$(CDIR)/AnyKernel3"
+
+mkdir builds-evert
+
+export OUT_DIR="/home/chanz22/Documentos/GitHub/android_kernel_motorola_msm8998/out"
+
+DATE_START=$(date +"%s")
+
+make O=out lineageos_evert_defconfig
+
+make O=out -j8
+
+IMAGE="$OUT_DIR/arch/arm64/boot/Image.gz-dtb"
+
+if [[ -f "$IMAGE" ]]; then
+    KERNELZIP="PNY_Kernel"
+
+    cp "$OUT_DIR"/arch/arm64/boot/Image.gz-dtb "$AK3"/Image.gz-dtb
+    # Create the AnyKernel package
+    (cd "AnyKernel3" && zip -r9 "$KERNELZIP" .) || error_exit "Error creating the AnyKernel package"
+
+    mv "$KERNELZIP" "$CDIR"/builds-evert/PNY.zip
+
+fi
+
+    DATE_END=$(date +"%s")
+    DIFF=$(($DATE_END - $DATE_START))
+
+    echo -e "\nCompilation completed successfully. Elapsed time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.\n"
